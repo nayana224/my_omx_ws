cb9e189c6ad751b24ae2c97bf96e42de6f27dd23
inpyo <inpyoi1304@gmail.com>
2025-11-12 03:17:51 +0900

    분기버전으로 mock 버전 완료. 코드 분석 필요함.

--- src/my_omx_bringup/launch/bringup.launch.py
+++ src/my_omx_bringup/launch/bringup.launch.py
@@ -1,3 +1,4 @@
+from typing import Mapping
 from setuptools import find_packages
 import controller_manager
 from launch import LaunchDescription
@@ -16,24 +17,49 @@ def generate_launch_description():
     description_pkg = FindPackageShare("my_omx_description").find("my_omx_description")
     bringup_pkg = FindPackageShare("my_omx_bringup").find("my_omx_bringup")
 
-    # URDF 로드
-    xacro_path = f"{description_pkg}/urdf/main.urdf.xacro"
-    doc = xacro.process_file(xacro_path)
-    robot_description = doc.toxml()
+    # ------------------------
+    # 런치 인자 선언
+    # ------------------------
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    declare_use_fake_hardware = DeclareLaunchArgument(
+        "use_fake_hardware",
+        default_value="true",
+        description="Use fake hardware or real Dynamixel hardware",
+    )
 
+    # ------------------------
+    # Xacro 로드 (Command 이용)
+    # ------------------------
+    xacro_path = f"{description_pkg}/urdf/main.urdf.xacro"
+    robot_description_content = Command(
+        [
+            "xacro ",
+            xacro_path,
+            " use_fake_hardware:=",
+            use_fake_hardware,
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    #  ------------------------
+    # 컨트롤러 설정
+    # ------------------------
     controller_config = PathJoinSubstitution([bringup_pkg, "config", "controller.yaml"])
 
+    # ------------------------
+    # Node 실행
+    # ------------------------
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}],
+        parameters=[robot_description],
         output="screen",
     )
 
     controller_manager_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{"robot_description": robot_description}, controller_config],
+        parameters=[robot_description, controller_config],
         output="screen",
     )
 
@@ -55,6 +81,7 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            declare_use_fake_hardware,
             robot_state_publisher_node,
             controller_manager_node,
             joint_state_broadcaster_spawner,
